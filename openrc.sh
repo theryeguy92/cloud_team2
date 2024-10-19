fec11f61175dbe122a9d49bf4d37a1786dcd6271
M. Findley <123705549+MFindley08@users.noreply.github.com>
2024-10-18 21:48:05 -0400

    Added clouds.yaml and openrc.sh files

---
+++ openrc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export OS_AUTH_URL=https://chi.uc.chameleoncloud.org:5000/v3
+export OS_PROJECT_ID=9c3848Cc69044cdfb5a8c7b5e273b276
+export OS_PROJECT_NAME="CH-<your-project-name>"  # Replace with your actual project name
+export OS_USER_DOMAIN_NAME="Default"
+export OS_USERNAME=michelle.l.findley@vanderbilt.edu
+export OS_PASSWORD=<your-password>  # Replace with your actual password
+export OS_REGION_NAME="RegionOne"
+export OS_INTERFACE="public"
+export OS_IDENTITY_API_VERSION=3
